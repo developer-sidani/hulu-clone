e74e1ccb8074c752bb1574ba3728d64d4b207e24
developer-sidani <asidani88@gmail.com>
2021-10-02 13:19:58 +0300

    draft - added the header item representing the icon and the title with style

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import Logo from './hulu-white.png'
+import { HeaderItem } from '..'
+import { headerItems } from '../../constants'
 
 const Header = () => (
     <header>
-      <h1 className="text-xl">Header</h1>
+      <div>
+      {/*  Header Items */}
+        {headerItems.map(({ _id, icon, title }) => (
+            <HeaderItem
+              key={_id}
+              Icon={icon}
+              title={title}
+            />
+        ))}
+      </div>
       <Image
         className="object-contain"
         src={Logo}
---
+++ constants/headerItems/index.js
@@ -0,0 +1,9 @@
+import { HomeIcon } from '@heroicons/react/outline'
+
+export const headerItems = [
+  {
+    _id: '1',
+    title: 'Home',
+    icon: HomeIcon,
+  },
+]
