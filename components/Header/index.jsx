e70739c735f18f8d9d0e52734ae9627528eaed2b
developer-sidani <asidani88@gmail.com>
2021-10-02 12:59:14 +0300

    fix - imported global.css to _app.jsx file

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -4,8 +4,13 @@ import Logo from './hulu-white.png'
 
 const Header = () => (
     <header>
-      <h1>Header</h1>
-      <Image src={Logo} width={200} height={100} />
+      <h1 className="text-xl">Header</h1>
+      <Image
+        className="object-contain"
+        src={Logo}
+        width={200}
+        height={100}
+      />
     </header>
 )
 
