ba7bca1930aca08607b6e99722d44381791eb4d2
developer-sidani <asidani88@gmail.com>
2021-10-16 02:18:23 +0300

    added ssr function to fetch from tmdb by genre

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import Head from 'next/head'
-import { Header, Nav } from '../components'
+import { Header, Nav, Results } from '../components'
+import { requests } from '../utils'
 
-const Home = () => (
-    <>
-      <Head>
-        <title>Hulu Clone | Sidani</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <Nav />
-      {/*  Results */}
-    </>
-)
+const tmdbBaseUrl = 'https://api.themoviedb.org/3'
+
+const Home = ({ results }) => {
+  console.log(results)
+  return (
+  <>
+    <Head>
+      <title>Hulu Clone | Sidani</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header />
+    <Nav />
+    <Results />
+  </>
+  )
+}
 
 export default Home
+
+export async function getServerSideProps(context) {
+  const { genre } = context.query
+  const req = await fetch(`${tmdbBaseUrl}${requests[genre]?.url || requests.fetchTrending.url}`)
+    .then((res) => res.json())
+
+  return {
+    props: {
+      results: req.results,
+    },
+  }
+}
