29a699a0ac9bf6731c139360139a7663532dc800
developer-sidani <asidani88@gmail.com>
2021-10-16 03:26:07 +0300

    final version of results

--- components/Results/index.jsx
+++ components/Results/index.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { ThumbNail } from '..'
 
 const Results = ({ results }) => (
-    <div>
+    <div className="px-5 my-10 sm:grid md:grid-cols-2
+    xl:grid-cols-3 3xl:flex flex-wrap justify-center"
+    >
       {results?.map(res => <ThumbNail key={res.id} res={res} />)}
     </div>
 )
