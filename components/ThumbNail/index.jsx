0f0d9c38eac88670ddbe1f0395bb2265b7c2f90b
developer-sidani <asidani88@gmail.com>
2021-10-16 03:13:15 +0300

    final version of the thumbnail

--- components/ThumbNail/index.jsx
+++ components/ThumbNail/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import { ThumbUpIcon } from '@heroicons/react/outline'
 import { tmdbImageUrl } from '../../constants'
 
 const ThumbNail = ({ res }) => (
-  <div>
+  <div className="p-2 group cursor-pointer
+  transition duration-200 ease-in transform
+  sm:hover:scale-105 hover:z-50"
+  >
     <Image
       src={`${tmdbImageUrl}${res?.backdrop_path || res?.poster_path}`
       || `${tmdbImageUrl}${res?.poster_path}`}
@@ -11,9 +15,22 @@ const ThumbNail = ({ res }) => (
       height={1080}
       width={1920}
     />
-    <div>
-      <p className="truncate">
+    <div className="p-2">
+      <p className="truncate max-w-md">
         {res?.overview}
+      </p>
+      <h2 className="mt-1 text-2xl transition-all
+      duration-100 ease-in-out group-hover:font-bold"
+      >
+        {res?.title || res?.original_name}
+      </h2>
+      <p className="flex items-center opacity-0 group-hover:opacity-100">
+        {res?.media_type && `${res?.media_type} •`}
+        {' '}
+        {res?.release_date || res?.first_air_date}
+        {' • '}
+        <ThumbUpIcon className="h-5 mx-2" />
+        {res?.vote_count}
       </p>
     </div>
   </div>
